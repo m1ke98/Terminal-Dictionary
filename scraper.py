034feb64b9a78c4d0247dba1a896befa5c31b8a1
Mike D'Argenio <mdargenio14@gmail.com>
2020-05-11 00:43:44 -0400

    update

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ options.add_argument('--headless')
 
 path = '/Users/michaeldargenio/projects/my-apps/py-scraper/chromedriver'
 driver = webdriver.Chrome(executable_path = path, chrome_options=options)
+print('----------\nAll definitions sourced from "https://www.lexico.com"\n----------\n')
 
 def define(word):
     url = 'https://www.lexico.com/en/definition/' + word
